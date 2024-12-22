c3a7e668c73fc08a98771ffa09b525d367c8a99b
AndChu123 <155886062+AndChu123@users.noreply.github.com>
2024-12-22 16:55:05 +0200

    logic for map

--- task1/asyncArray.js
+++ task1/asyncArray.js
@@ -1,3 +1,12 @@
 const asyncMap = async (array, callback, debounceTime = 1000) => {
+  const startTime = Date.now();
 
-}
+  const results = await Promise.all(
+    array.map(async (item, index, array) => {
+      const result = await callback(item, indexm array);
+      return result;
+    })
+  );
+
+  return results;
+};
