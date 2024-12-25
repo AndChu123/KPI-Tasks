853434952978d75f4e67957fafb85b4267dedb5f
AndChu123 <155886062+AndChu123@users.noreply.github.com>
2024-12-25 22:25:02 +0200

    metod subscribe

--- task5/EventEmitter.js
+++ task5/EventEmitter.js
@@ -2,4 +2,13 @@ class EventEmitter {
   constructor() {
     this.subscribers = new Map();
   }
+
+  subscribe(topic, callback){
+    if(!this.subscribers.has(topic)) {
+      this.subscribers.set(topic, new Set());
+  }
+    this.subscribers.get(topic).add(callback);
+
+    return() => this.unsubscribe(topic, callback);
+  }
 }
