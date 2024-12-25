51de6610e97675090d2fff7706750cfd88958b39
AndChu123 <155886062+AndChu123@users.noreply.github.com>
2024-12-25 20:39:26 +0200

    added AsyncIterator

--- task4/AsyncIterator.js
+++ task4/AsyncIterator.js
@@ -4,3 +4,13 @@ class DataStream {
     this.batchsize = batchSize;
   }
 }
+
+async *[Symbol.asyncIterator]() {
+  let offset = 0;
+  while(true) {
+    const batch = await this.source.slice(offset, offset + this.batchSize);
+    if (batch.length === 0) break;
+    yield* batch;
+    offset += this.batchSize;
+  }
+}
