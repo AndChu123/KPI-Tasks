e4de5bc40afdfec7b226963b9074ea01431099a2
AndChu123 <155886062+AndChu123@users.noreply.github.com>
2024-12-25 15:09:38 +0200

    basis from the first task

---
+++ task2/asyncMap with parallelism.js
@@ -0,0 +1,41 @@
+const asyncMap = async (array, callback, debounceTime = 1000) => {
+  const startTime = Date.now();
+
+  const results = await Promise.all(
+    array.map(async (item, index, array) => {
+      const result = await callback(item, index, array);
+      return result;
+    })
+  );
+
+const executionTime = Date.now() - startTime;
+if (executionTime < debounceTime) {
+  await new Promise(resolve =>
+    setTimeout(resolve, debounceTime - executionTime)
+                    );
+}
+
+return results;
+};
+
+const demo = async () => {
+  const numbers = [1,2,3,4,5];
+
+  console.log('Test 1');
+  const multipliedNumbers = await asyncMap(numbers, async (num) => {
+    await new Promise(resolve => setTimeout(resolve, 100));
+    return num*2;
+  });
+  console.log('result', multipliedNumbers);
+
+  console.log('\nTest with debounce')
+  console.time('debounceTest');
+  const processedData = await asyncMap(numbers, async (num) => {
+    return 'Processed ${num}`;
+  }, 2000);
+
+  console.timeEnd('debounceTest');
+  console.log('result', processedData);
+};
+
+demo().catch(console.error);
