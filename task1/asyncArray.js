9fb5a67fd9ab444a6fd0c19860a1c38dee1f998f
AndChu123 <155886062+AndChu123@users.noreply.github.com>
2024-12-28 18:10:14 +0200

    added new asyncMap

--- task1/asyncArray.js
+++ task1/asyncArray.js
@@ -1,1 +1,32 @@
+const asyncMap = (array, iteratee, callback, debounceTime = 1000) => {
+  const startTime = Date.now();
+  let completed = 0;
+  const results = new Array(array.length);
+  let hasError = fallse;
 
+  array.forEach((item, index) => {
+    iteratee(item, (error, result) => {
+      if(hasError) return;
+
+      if(error) {
+        hasError = true;
+        return callback(error);
+      }
+
+      results[index] = result;
+      completed++;
+
+      if (completed === array.length) {
+        const executionTime = Date.now() - startTime;
+
+        if(executionTime < debounceTime) {
+          setTimeout(() => {
+            callback(null, results);
+          }, debounceTime - executionTime);
+        } else {
+          callback(null, results);
+        }
+      }
+    });
+  });
+};
